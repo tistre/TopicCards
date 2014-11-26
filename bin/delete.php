b1d2b1f59785dbb053366308c73ec077f883a5dd
Tim Strehle <tim@strehle.de>
2014-11-26 20:38:53 +0000

    bin/delete.php can now read from stdin.

--- bin/delete.php
+++ bin/delete.php
@@ -2,6 +2,69 @@
 
 require_once dirname(__DIR__) . '/include/config.php';
 
+
+function deleteObject($id)
+{
+    global $options;
+    
+    if ($options[ 'topics' ])
+    {
+        deleteTopic($id);
+    }
+    elseif ($options[ 'associations' ])
+    {
+        deleteAssociation($id);
+    }
+}
+
+
+function deleteTopic($topic_id)
+{
+    global $topicmap;
+    global $options;
+    
+    $topic = $topicmap->newTopic();
+    $topic->load($topic_id);
+    
+    if ($options[ 'with-associations' ])
+    {
+        foreach ($topicmap->getAssociations([ 'role_player' => $topic_id ]) as $association_id)
+            deleteAssociation($association_id);
+    }
+    
+    $ok = $topic->delete();
+
+    printf("Deleted topic <%s> (%s)\n", $topic_id, $ok);
+}
+
+
+function deleteAssociation($association_id)
+{
+    global $topicmap;
+    global $options;
+    
+    $association = $topicmap->newAssociation();
+    $reifier = $topicmap->newTopic();
+    
+    $association->load($association_id);
+        
+    $reifier_id = $association->getReifier();
+ 
+    $ok = $association->delete();
+            
+    printf("Deleted association <%s> (%s)\n", $association_id, $ok);
+        
+    if ($options[ 'with-reifiers' ] && (strlen($reifier_id) > 0))
+    {
+        $reifier->load($reifier_id);
+
+        $ok = $reifier->delete();
+    
+        printf("Deleted reifier %s (%s)\n", $reifier_id, $ok);
+    }
+}
+
+
 // XXX use proper CLI argument parser
 
 $ids = [ ];
@@ -28,64 +91,22 @@ foreach ($argv as $i => $arg)
     $ids[ ] = $arg;
 }
 
-$topic = $topicmap->newTopic();
-$reifier = $topicmap->newTopic();
-$association = $topicmap->newAssociation();
-
-if ($options[ 'topics' ])
+if (in_array('-', $ids, true))
+{
+    while (! feof(STDIN))
+    {
+        $id = trim(fgets(STDIN));
+        
+        if ($id === '')
+            continue;
+            
+        deleteObject($id);
+    }
+}
+else
 {
     foreach ($ids as $topic_id)
     {
-        $topic->load($topic_id);
-    
-        if ($options[ 'with-associations' ])
-        {
-            foreach ($topicmap->getAssociations([ 'role_player' => $topic_id ]) as $association_id)
-            {
-                $association->load($association_id);
-        
-                $reifier_id = $association->getReifier();
-        
-                $ok = $association->delete();
-            
-                printf("Deleted association %s (%s)\n", $association_id, $ok);
-            
-                if ($options[ 'with-reifiers' ] && (strlen($reifier_id) > 0))
-                {
-                    $reifier->load($reifier_id);
-
-                    $ok = $reifier->delete();
-        
-                    printf("Deleted reifier %s (%s)\n", $reifier_id, $ok);
-                }
-            }
-        }
-    
-        $ok = $topic->delete();
-    
-        printf("Deleted topic %s (%s)\n", $topic_id, $ok);
+        deleteObject($id);
     }
 }
-elseif ($options[ 'associations' ])
-{
-    foreach ($ids as $association_id)
-    {
-        $association->load($association_id);
-        
-        $reifier_id = $association->getReifier();
-     
-        $ok = $association->delete();
-            
-        printf("Deleted association %s (%s)\n", $association_id, $ok);
-            
-        if ($options[ 'with-reifiers' ] && (strlen($reifier_id) > 0))
-        {
-            $reifier->load($reifier_id);
-
-            $ok = $reifier->delete();
-        
-            printf("Deleted reifier %s (%s)\n", $reifier_id, $ok);
-        }
-    }
-}
-
