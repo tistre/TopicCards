9d57817cfa53537563425f0ec5e9fab419295b42
Tim Strehle <tim@strehle.de>
2014-12-02 17:01:00 +0000

    bin/topic_id_to_subject.php tool

---
+++ bin/topic_id_to_subject.php
@@ -0,0 +1,42 @@
+<?php
+
+use Ulrichsg\Getopt\Getopt;
+use Ulrichsg\Getopt\Option;
+
+require_once dirname(__DIR__) . '/include/config.php';
+
+
+$getopt = new Getopt(
+[
+    new Option('h', 'help')
+]);
+
+$getopt->parse();
+
+if ($getopt[ 'help' ])
+{
+    $getopt->setBanner("\nTopicBank topic ID to subject identifier\n\n");
+    
+    echo $getopt->getHelpText();
+    exit;
+}
+
+if ($getopt->getOperand(0) === '-')
+{
+    while (! feof(STDIN))
+    {
+        $id = trim(fgets(STDIN));
+        
+        if ($id === '')
+            continue;
+            
+        echo $topicmap->getTopicSubjectIdentifier($id) . "\n";
+    }
+}
+else
+{
+    foreach ($getopt->getOperands() as $id)
+    {
+        echo $topicmap->getTopicSubjectIdentifier($id) . "\n";
+    }
+}
