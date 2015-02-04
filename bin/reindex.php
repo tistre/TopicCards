c2118124f2a33c47b3ed766b115d591de8d51d2d
Tim Strehle <tim@strehle.de>
2015-02-04 20:34:58 +0000

    Refactoring bin/reindex.php. --index and --limit parameters added.

--- bin/reindex.php
+++ bin/reindex.php
@@ -1,196 +1,273 @@
 <?php
 
+namespace TopicBank\Bin;
+
+use \Ulrichsg\Getopt\Getopt;
+use \Ulrichsg\Getopt\Option;
+
 require_once dirname(__DIR__) . '/include/config.php';
 
-$services->search->init();
 
-$elasticsearch = $services->search->getElasticSearchClient();
-
-// Recreate index (XXX make this optional)
-
-if ($elasticsearch->indices()->exists([ 'index' => $topicmap->getSearchIndex() ]))
+class Reindex
 {
-    printf("Deleting index %s...\n", $topicmap->getSearchIndex());
-
-    $response = $elasticsearch->indices()->delete([ 'index' => $topicmap->getSearchIndex() ]);
-
-    print_r($response);
-}
-
-$params =
-[
-    'index' => $topicmap->getSearchIndex(),
-    'body' => 
-    [
-        'mappings' =>
+    protected $topicmap;
+    protected $getopt;
+    
+    
+    public function __construct(\TopicBank\Interfaces\iTopicMap $topicmap)
+    {
+        $this->topicmap = $topicmap;
+    }
+    
+    
+    public function execute()
+    {
+        $this->getopt = new Getopt(
         [
-            'topic' =>
+            new Option('i', 'index', Getopt::REQUIRED_ARGUMENT),
+            new Option('l', 'limit', Getopt::REQUIRED_ARGUMENT),
+            new Option('h', 'help')
+        ]);
+
+        $this->getopt->parse();
+    
+        if ($this->getopt[ 'help' ])
+        {
+            $this->getopt->setBanner("\nTopicBank Elasticsearch reindex\n\n");
+    
+            echo $this->getopt->getHelpText();
+            exit;
+        }
+        
+        $start_time = microtime(true);
+        
+        $index = $this->topicmap->getSearchIndex();
+        
+        if ($this->getopt[ 'index' ])
+            $index = $this->getopt[ 'index' ];
+        
+        $this->recreateIndex
+        (
+            $index, 
+            $this->getIndexParams($index)
+        );
+        
+        $this->indexTopics($topic_summary);
+        $this->indexAssociations($association_summary);
+        
+        echo "Done.\n";
+
+        echo $topic_summary . $association_summary;
+
+        $seconds = (microtime(true) - $start_time);
+        $minutes = ($seconds / 60);
+
+        printf("Total time: %.1f s = %d minutes\n", $seconds, $minutes);
+    }
+    
+    
+    protected function getIndexParams($index)
+    {
+        $params =
+        [
+            'index' => $index,
+            'body' => 
             [
-                '_source' => [ 'enabled' => true ],
-                'properties' => 
+                'mappings' =>
                 [
-                    'label' => 
-                    [ 
-                        'type' => 'string',
-                        'fields' =>
+                    'topic' =>
+                    [
+                        '_source' => [ 'enabled' => true ],
+                        'properties' => 
                         [
-                            'raw' => 
-                            [
+                            'label' => 
+                            [ 
+                                'type' => 'string',
+                                'fields' =>
+                                [
+                                    'raw' => 
+                                    [
+                                        'type' => 'string',
+                                        'index' => 'not_analyzed'
+                                    ]
+                                ]
+                            ],
+                            'name' => 
+                            [ 
+                                'type' => 'string'
+                            ],
+                            'has_name_type' => 
+                            [ 
+                                'type' => 'string',
+                                'index' => 'not_analyzed'
+                            ],
+                            'topic_type' => 
+                            [ 
+                                'type' => 'string',
+                                'index' => 'not_analyzed'
+                            ],
+                            'subject' => 
+                            [ 
+                                'type' => 'string',
+                                'index' => 'not_analyzed'
+                            ],
+                            'occurrence' => 
+                            [ 
+                                'type' => 'string'
+                            ],
+                            'has_occurrence_type' => 
+                            [ 
                                 'type' => 'string',
                                 'index' => 'not_analyzed'
                             ]
                         ]
                     ],
-                    'name' => 
-                    [ 
-                        'type' => 'string'
-                    ],
-                    'has_name_type' => 
-                    [ 
-                        'type' => 'string',
-                        'index' => 'not_analyzed'
-                    ],
-                    'topic_type' => 
-                    [ 
-                        'type' => 'string',
-                        'index' => 'not_analyzed'
-                    ],
-                    'subject' => 
-                    [ 
-                        'type' => 'string',
-                        'index' => 'not_analyzed'
-                    ],
-                    'occurrence' => 
-                    [ 
-                        'type' => 'string'
-                    ],
-                    'has_occurrence_type' => 
-                    [ 
-                        'type' => 'string',
-                        'index' => 'not_analyzed'
-                    ]
-                ]
-            ],
-            'association' => 
-            [
-                '_source' => [ 'enabled' => true ],
-                'properties' => 
-                [
-                    'association_type' => 
-                    [ 
-                        'type' => 'string',
-                        'index' => 'not_analyzed'
-                    ],
-                    'has_role_type' => 
-                    [ 
-                        'type' => 'string',
-                        'index' => 'not_analyzed'
-                    ],
-                    'has_player_id' => 
-                    [ 
-                        'type' => 'string',
-                        'index' => 'not_analyzed'
+                    'association' => 
+                    [
+                        '_source' => [ 'enabled' => true ],
+                        'properties' => 
+                        [
+                            'association_type' => 
+                            [ 
+                                'type' => 'string',
+                                'index' => 'not_analyzed'
+                            ],
+                            'has_role_type' => 
+                            [ 
+                                'type' => 'string',
+                                'index' => 'not_analyzed'
+                            ],
+                            'has_player_id' => 
+                            [ 
+                                'type' => 'string',
+                                'index' => 'not_analyzed'
+                            ]
+                        ]
                     ]
                 ]
             ]
-        ]
-    ]
-];
+        ];
 
-$callback_result = [ ];
+        $callback_result = [ ];
 
-$topicmap->trigger
-(
-    \TopicBank\Backends\Db\Search::EVENT_INDEX_PARAMS, 
-    [ 'index_params' => $params ],
-    $callback_result
-);
+        $this->topicmap->trigger
+        (
+            \TopicBank\Backends\Db\Search::EVENT_INDEX_PARAMS, 
+            [ 'index_params' => $params ],
+            $callback_result
+        );
 
-if (isset($callback_result[ 'index_params' ]) && is_array($callback_result[ 'index_params' ]))
-    $params = $callback_result[ 'index_params' ];
-
-printf("Creating index %s...\n", $topicmap->getSearchIndex());
-
-$response = $elasticsearch->indices()->create($params);
-
-print_r($response);
-
-$limit = 0;
-
-echo "Indexing topics...\n";
-
-$topic = $topicmap->newTopic();
-$cnt = 0;
-$topic_start_time = microtime(true);
-
-foreach ($topicmap->getTopicIds([ ]) as $topic_id)
-{
-    $ok = $topic->load($topic_id);
+        if (isset($callback_result[ 'index_params' ]) && is_array($callback_result[ 'index_params' ]))
+            $params = $callback_result[ 'index_params' ];
     
-    if ($ok >= 0)
-        $ok = $topic->index();
+        return $params;
+    }
     
-    printf("#%d %s (%s)\n", ++$cnt, $topic->getId(), $ok);
     
-    if (($limit > 0) && ($cnt >= $limit))
-        break;
+    protected function recreateIndex($index, $params)
+    {
+        $services = $this->topicmap->getServices();
+        
+        $services->search->init();
+
+        $elasticsearch = $services->search->getElasticSearchClient();
+
+        if ($elasticsearch->indices()->exists([ 'index' => $index ]))
+        {
+            printf("Deleting index %s...\n", $index);
+
+            $response = $elasticsearch->indices()->delete([ 'index' => $index ]);
+
+            print_r($response);
+        }
+
+        printf("Creating index %s...\n", $index);
+
+        $response = $elasticsearch->indices()->create($params);
+
+        print_r($response);
+    }
+
+
+    protected function indexTopics(&$summary)
+    {
+        $limit = 0;
+        
+        if ($this->getopt[ 'limit' ])
+            $limit = $this->getopt[ 'limit' ];
+
+        echo "Indexing topics...\n";
+
+        $topic = $this->topicmap->newTopic();
+        $cnt = 0;
+        $start_time = microtime(true);
+
+        foreach ($this->topicmap->getTopicIds([ ]) as $topic_id)
+        {
+            $ok = $topic->load($topic_id);
+    
+            if ($ok >= 0)
+                $ok = $topic->index();
+    
+            printf("#%d %s (%s)\n", ++$cnt, $topic->getId(), $ok);
+    
+            if (($limit > 0) && ($cnt >= $limit))
+                break;
+        }
+
+        $summary = $this->formatSummary('topic', $cnt, $start_time);
+    }
+
+
+    protected function indexAssociations(&$summary)
+    {
+        $limit = 0;
+        
+        if ($this->getopt[ 'limit' ])
+            $limit = $this->getopt[ 'limit' ];
+
+        echo "Indexing associations...\n";
+
+        $association = $this->topicmap->newAssociation();
+        $cnt = 0;
+        $start_time = microtime(true);
+
+        foreach ($this->topicmap->getAssociationIds([ ]) as $association_id)
+        {
+            $ok = $association->load($association_id);
+    
+            if ($ok >= 0)
+                $ok = $association->index();
+    
+            printf("#%d %s (%s)\n", ++$cnt, $association->getId(), $ok);
+    
+            if (($limit > 0) && ($cnt >= $limit))
+                break;
+        }
+
+        $summary = $this->formatSummary('association', $cnt, $start_time);
+    }
+    
+    
+    protected function formatSummary($type, $cnt, $start_time)
+    {
+        $total_time = (microtime(true) - $start_time);
+
+        if ($cnt === 0)
+            return sprintf("No %ss indexed.\n", $type);
+
+        return sprintf
+        (
+            "%d %ss indexed in %.1f s (%.3f s per %s).\n",
+            $cnt,
+            $type,
+            $total_time,
+            ($total_time / $cnt),
+            $type
+        );
+    }
 }
 
-$total_time = (microtime(true) - $topic_start_time);
 
-if ($cnt === 0)
-{
-    echo "No topics indexed.\n";
-}
-else
-{
-    $topic_summary = sprintf
-    (
-        "%d topics indexed in %.1f s (%.3f s per topic).\n",
-        $cnt,
-        $total_time,
-        ($total_time / $cnt)
-    );
-}
-
-echo "Indexing associations...\n";
-
-$association = $topicmap->newAssociation();
-$cnt = 0;
-$association_start_time = microtime(true);
-
-foreach ($topicmap->getAssociationIds([ ]) as $association_id)
-{
-    $ok = $association->load($association_id);
-    
-    if ($ok >= 0)
-        $ok = $association->index();
-    
-    printf("#%d %s (%s)\n", ++$cnt, $association->getId(), $ok);
-    
-    if (($limit > 0) && ($cnt >= $limit))
-        break;
-}
-
-$total_time = (microtime(true) - $association_start_time);
-
-if ($cnt === 0)
-{
-    $association_summary = "No associations indexed.\n";
-}
-else
-{
-    $association_summary = sprintf
-    (
-        "%d associations indexed in %.1f s (%.3f s per association).\n",
-        $cnt,
-        $total_time,
-        ($total_time / $cnt)
-    );
-}
-
-echo "Done.\n";
-
-echo $topic_summary . $association_summary;
-
-printf("Total time: %d s\n", (microtime(true) - $topic_start_time));
+$main = new Reindex($topicmap);
+$main->execute();
