5062cb52e695c5260cffa74c27e2c796071ed1f1
Tim Strehle <tim.strehle@spiegel.de>
2022-03-03 23:17:05 +0100

    Add first controller

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -2,18 +2,42 @@
 
 namespace App\Controller;
 
+use Laudis\Neo4j\Types\CypherMap;
+use StrehleDe\TopicCards\Configuration\Configuration;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/search")
      * @return Response
      */
-    public function search(): Response
+    public function search(Configuration $topicCardsConfig): Response
     {
-        return new Response('hello world');
+        $neo4jClient = $topicCardsConfig->getNeo4jConfig()->getClient();
+
+        $statement = <<<'EOT'
+            MATCH (o:Organization) RETURN o.name, o.url LIMIT 20
+            EOT;
+
+        $rows = $neo4jClient->run($statement);
+
+        if (count($rows) < 1) {
+            return new Response('No Organization found in database');
+        }
+
+        $html = '<body><h1>Organizations</h1><ul>';
+
+        /** @var CypherMap $row */
+        foreach ($rows as $row) {
+            $html .= sprintf("<li>%s</li>\n", $row->get('o.name'));
+        }
+
+        $html .= '</ul>';
+
+        return new Response($html);
     }
 }
